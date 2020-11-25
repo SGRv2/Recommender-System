f5940f446f8c78e5d7328d604354e216caa4e770
ahsanabbas123 <saa5819@gmail/com>
2020-11-25 15:31:07 +0530

    added Report.docx.pdf

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ Outputs : list() Mean Average Error for the 5 folds
 def KFold_evaluate(ratings, ratings_train, content_boosted, movies):
   MAE = [] # store mean absolute error for each run 
   if content_boosted: print ("Starting evaluation with content boosting")
-  topN = 75 if content_boosted else 75
+  topN = 50 if content_boosted else 75
   kf = KFold(n_splits = 5, shuffle = True, random_state = 2)  # 5 fold 
   j = 0 # track split number
   for training, testing in kf.split(ratings_train):
